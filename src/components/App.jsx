b53ac329bab36f8316d9c94cb7c3438f854727d0
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-09-16 15:20:46 +0300

    ex1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import { Profile } from "./Profile";
-import user from "../json/user.json";
+import { Profile } from './Profile/Profile';
+import user from '../json/user.json';
 
 export const App = () => {
   return (
-    <Profile 
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
     />
   );
 };
