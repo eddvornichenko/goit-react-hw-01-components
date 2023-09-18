cc72fac96333c40f034ad7db725f1e836baa1756
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-09-18 16:21:42 +0300

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from './Profile/Profile';
 import user from '../json/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../json/data.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      
+    </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import css from './statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.stats_list}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li className={`${css.stats} ${css[label.slice(1)]}`} key={id}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
